591d85f1db4f78d58ec0d41a86bbd04658a5b68b
waybeforenow <wbn@striated.space>
2020-12-15 14:48:08 +0900

    add some balance watcher tests

--- client_test.py
+++ client_test.py
@@ -1,8 +1,27 @@
 import unittest
+from unittest.mock import Mock
+
+from eth.v1alpha1 import beacon_chain_pb2, beacon_chain_pb2_grpc
+from client import BalanceWatcher
 
 
 class BalanceWatcherTest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.alerter_mock = Mock()
+        self.stub_mock = Mock()
+
+    def test_generate_messages(self):
+        bw = BalanceWatcher(self.stub_mock, self.alerter_mock, [])
+        messages = bw._generate_messages()
+        self.assertEqual(
+            next(messages),
+            beacon_chain_pb2.ValidatorChangeSet(
+                action=beacon_chain_pb2.SET_VALIDATOR_KEYS,
+                public_keys=[],
+            ),
+        )
+        with self.assertRaises(StopIteration):
+            next(messages)
 
 
 class ClientTest(unittest.TestCase):
