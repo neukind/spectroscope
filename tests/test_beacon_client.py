99abac29f40950a92ce54bbdbf9aa07ef91422f4
waybeforenow <wbn@striated.space>
2020-12-28 14:51:47 +0900

    100% coverage for beacon streamer

--- tests/test_beacon_client.py
+++ tests/test_beacon_client.py
@@ -1,11 +1,16 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, call
 from nose2.tools import params
 
-from ethereumapis.v1alpha1 import beacon_chain_pb2
+from ethereumapis.v1alpha1 import beacon_chain_pb2, validator_pb2
 from spectroscope.beacon_client import BeaconChainStreamer
-from spectroscope.model.base import Event
-from spectroscope.module import ConfigOption, Plugin, Subscriber
+from spectroscope.model.base import ChainTimestamp, Event, ValidatorIdentity
+from spectroscope.model.update import (
+    ValidatorBalanceUpdate,
+    ValidatorStatusUpdate,
+    UpdateBatch,
+)
+from spectroscope.module import ConfigOption, Module, Plugin, Subscriber
 from typing import List, Type, Union
 
 
@@ -13,11 +18,12 @@ def TestModuleFactory(
     module_type: Union[Type[Plugin], Type[Subscriber]],
     init: Mock = Mock(),
     consume: Mock = Mock(),
+    consumed_types: List[Event] = [],
 ):
     class TestModule(module_type):
         config_options: List[ConfigOption] = []
 
-        _consumed_types: List[Event] = []
+        _consumed_types: List[Event] = consumed_types
 
         def __init__(self, **kwargs):
             init(**kwargs)
@@ -55,7 +61,7 @@ class BeaconChainStreamerTest(unittest.TestCase):
         with self.assertRaises(StopIteration):
             next(generator)
 
-    @params([])
+    @params([], [bytes.fromhex("a" * 96)])
     def test_generate_messages(self, validator_set):
         bcs = BeaconChainStreamer(self.stub_mock, [])
         bcs.add_validators(validator_set)
@@ -66,6 +72,37 @@ class BeaconChainStreamerTest(unittest.TestCase):
         vcs_generator = self.stub_mock.StreamValidatorsInfo.call_args[0][0]
         self._assert_validator_change_set(vcs_generator, validator_set)
 
+    def test_stream_messages_e2e(self):
+        consume_mock = Mock()
+        consume_mock.return_value = []
+        subscriber = TestSubscriberFactory(
+            consume=consume_mock,
+            consumed_types=[ValidatorStatusUpdate, ValidatorBalanceUpdate],
+        )
+        plugin = TestPluginFactory()
+        self.stub_mock.StreamValidatorsInfo.return_value = [
+            validator_pb2.ValidatorInfo(
+                public_key=bytes.fromhex("a" * 96),
+                index=60,
+                balance=300,
+                epoch=123,
+                status=2,
+            )
+        ]
+
+        bcs = BeaconChainStreamer(self.stub_mock, [(subscriber, {}), (plugin, {})])
+        bcs.stream()
+        consume_mock.assert_called_once_with(
+            UpdateBatch(
+                validator=ValidatorIdentity(idx=60, pubkey=bytes.fromhex("a" * 96)),
+                timestamp=ChainTimestamp(epoch=123, slot=0, timestamp=None),
+                updates=[
+                    ValidatorStatusUpdate(status=2),
+                    ValidatorBalanceUpdate(balance=300),
+                ],
+            )
+        )
+
     def test_subscriber_activation(self):
         init_mock = Mock()
         module_set = [(TestSubscriberFactory(init=init_mock), {})]
@@ -73,11 +110,28 @@ class BeaconChainStreamerTest(unittest.TestCase):
         init_mock.assert_called_once()
 
     def test_plugin_activation(self):
-        module_set = [(TestPluginFactory(init=Mock(), consume=Mock()), {})]
+        init_mock = Mock()
+        module_set = [(TestPluginFactory(init=init_mock), {})]
         bcs = BeaconChainStreamer(self.stub_mock, module_set)
+        init_mock.assert_called_once()
 
-    def test_add_validators(self):
-        pass
+    def test_module_rejection(self):
+        module_set = [(Module, {})]
+        with self.assertRaises(TypeError):
+            bcs = BeaconChainStreamer(self.stub_mock, module_set)
 
-    def test_remove_validators(self):
-        pass
+    def test_add_remove_validators(self):
+        validator_one = bytes.fromhex("a" * 96)
+        validator_two = bytes.fromhex("b" * 96)
+
+        bcs = BeaconChainStreamer(self.stub_mock, [])
+        bcs.add_validators([validator_one])
+        self.assertSetEqual(bcs.validator_set, set([validator_one]))
+        bcs.add_validators([validator_two])
+        self.assertSetEqual(bcs.validator_set, set([validator_one, validator_two]))
+        bcs.remove_validators([validator_one])
+        self.assertSetEqual(bcs.validator_set, set([validator_two]))
+        bcs.remove_validators([validator_two])
+        self.assertSetEqual(bcs.validator_set, set())
+        with self.assertRaises(KeyError):
+            bcs.remove_validators([validator_one])
