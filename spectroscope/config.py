e534bf69b7eda9ec9dea59a58532bce9ec761d29
waybeforenow <wbn@striated.space>
2021-01-28 17:16:03 +0900

    fix default value for pubkeys

--- spectroscope/config.py
+++ spectroscope/config.py
@@ -16,10 +16,7 @@ SYSTEM_MODULE_CONFIG = {
             name="pubkeys",
             param_type=list,
             description="List of hex-encoded public keys to watch",
-            default=[
-                "0x933ad9491b62059dd065b560d256d8957a8c402cc6e8d8ee7290ae11e8f7329267a8811c397529dac52ae1342ba58c95",
-                "0x8a30c341a1b23abc54affb82c13a71eaf69d770debea914ec2f8f26b6c762b0da2a5878c33e75a46602c7116b70161a2",
-            ],
+            default=[],
         ),
     ]
 }
