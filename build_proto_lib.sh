0bf1f879d31132e8c1e5dcad9fbb2c9f00639bab
HikaruG <hikaru.goto@neukind.jp>
2021-06-24 15:32:56 +0900

    last commit before hard reset

--- build_proto_lib.sh
+++ build_proto_lib.sh
@@ -3,14 +3,14 @@ set -e
 #cd "$(dirname "$0")"
 
 # create directory to store compiled output
-#mkdir -p proto
+mkdir -p proto
 # creating a directory was messing around the import of the service_pb2 thus I deleted it.
 # now the proto python files are created in the working directory, thus the naas_proto is being created automatically
 
 # build necessary Python libraries for gRPC from protobuf definitions
 python -m grpc_tools.protoc \
     -Iinclude/spectroscope-proto \
-    --python_out=${PWD} \
-    --grpc_python_out=${PWD} \
+    --python_out=${PWD}/proto \
+    --grpc_python_out=${PWD}/proto \
     include/spectroscope-proto/validator/service.proto \
     include/spectroscope-proto/validator/node.proto 
--- spectroscope/spectroscope_client.py
+++ spectroscope/spectroscope_client.py
@@ -1,22 +1,17 @@
 import spectroscope
-from ethereumapis.v1alpha1 import validator_pb2, validator_pb2_grpc
-from spectroscope.model import ChainTimestamp, ValidatorIdentity
-from spectroscope.model.update import (
-    ValidatorActivationUpdate,
-    ActivationBatch,
-)
 from spectroscope.exceptions import Interrupt, UserInteraction
-from spectroscope.module import Module, Plugin, Subscriber
 from typing import List, Set, Tuple, Type
 import asyncio
 import grpc
 from concurrent import futures
+from proto import service_pb2_grpc
 
 
 def serve():
   server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-  route_guide_pb2_grpc.add_RouteGuideServicer_to_server(
-      RouteGuideServicer(), server)
+  service_pb2_grpc.add_ValidatorServiceServicer_to_server(
+      service_pb2_grpc.ValidatorServiceServicer,server
+    )
   server.add_insecure_port('[::]:50051')
   server.start()
   server.wait_for_termination()
