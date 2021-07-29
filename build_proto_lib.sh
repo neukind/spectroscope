90703aec4caadee7dda6c419091cb9b77921edda
HikaruG <hikaru.goto@neukind.jp>
2021-07-29 22:14:06 +0900

    updated build path

--- build_proto_lib.sh
+++ build_proto_lib.sh
@@ -4,7 +4,7 @@ set -e
 # build necessary Python libraries for gRPC from protobuf definitions
 python -m grpc_tools.protoc \
     -Iproto_files/validator \
-    --python_out=${PWD} \
-    --grpc_python_out=${PWD} \
+    --python_out=proto_files/validator \
+    --grpc_python_out=proto_files/validator \
     proto_files/validator/service.proto \
     proto_files/validator/node.proto 
